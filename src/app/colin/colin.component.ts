1f0f5dd659555fce7edb512cc57fff9c2108925f
Colin Smith <colinxsmith@gmail.com>
2018-04-21 01:11:46 +0100

    Add d3 can't make it work for rectangles with css class

--- src/app/colin/colin.component.ts
+++ src/app/colin/colin.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+import * as d3 from 'd3';
 @Component({
   selector: 'app-colin',
   templateUrl: './colin.component.html',
-  styleUrls: ['./colin.component.css']
+  styleUrls: ['./colin.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ColinComponent implements OnInit {
+  BAR() {
+    const svg = d3
+      .select('app-colin')
+      .append('svg')
+      .attr('viewBox', '0 0 500 500')
+      .append('g');
 
-  constructor() { }
+    svg.append('circle')
+      .attr('r', '50px')
+      .attr('cx', '100px')
+      .attr('cy', '100px')
+      .attr('class', 'rim');
 
-  ngOnInit() {
+    svg.append('rect')
+    //.attr('class', 'rim')
+    .style('stroke', 'green')
+    .style('stroke-width', '10px')
+    .style('fill', 'brown')
+    .attr('transform', 'translate(200,100)')
+    .attr('height', '50px')
+    .attr('width', '50px');
   }
 
+  constructor() {}
+
+  ngOnInit() {
+    this.BAR();
+  }
 }
