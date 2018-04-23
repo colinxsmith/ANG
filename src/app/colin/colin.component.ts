af5deeabeab1c851e6d892c5210641ee62fb346b
Colin Smith <colin.smith@corfinancialgroup.com>
2018-04-23 10:08:34 +0100

    Base version

--- src/app/colin/colin.component.ts
+++ src/app/colin/colin.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, ViewEncapsulation, OnChanges} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import * as d3 from 'd3';
 @Component({
   selector: 'app-colin',
   templateUrl: './colin.component.html',
-  styleUrls: ['./colin.component.css'],
+  styleUrls: ['./colin.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ColinComponent implements OnInit, OnChanges {
+export class ColinComponent implements OnInit {
   centX = '100px';
   centY = '100px';
   BAR() {
@@ -22,42 +22,22 @@ export class ColinComponent implements OnInit, OnChanges {
       .attr('cx', this.centX)
       .attr('cy', this.centY)
       .attr('class', 'rim')
-      .on('mousemove' && 'mousedown', function(d, i, hh: d3.BaseType[]) {
-       function distAB(x: number[], y: number[]) {
-          return Math.sqrt((x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1]));
-        }
-        const c1: number[] = [];
-        const mouseh = d3.mouse(d3.event.currentTarget);
-        console.log(mouseh[0] + ' ' + mouseh[1]);
-        const circle = d3.select(hh[i]);
-        c1[0] = +circle.attr('cx').replace('px', '');
-        c1[1] = +circle.attr('cy').replace('px', '');
-        if ( distAB(c1, mouseh) <= 10) {
-          console.log('here');
-        circle.attr('cx', mouseh[0]);
-        circle.attr('cy', mouseh[1]);
-        }
-      })
       ;
 
     svg
       .append('rect')
-      // .attr('class', 'rim')
-      .style('stroke', 'green')
-      .style('stroke-width', '10px')
-      .style('fill', 'brown')
+      .attr('class', 'rim')
+      //  .style('stroke', 'green')
+      //  .style('stroke-width', '10px')
+      //  .style('fill', 'brown')
       .attr('transform', 'translate(200,100)')
       .attr('height', '50px')
       .attr('width', '50px');
   }
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
     this.BAR();
-  }
-
-  ngOnChanges() {
-    console.log('Onchanges');
   }
 }
